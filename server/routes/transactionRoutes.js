7f349d509efef2d6ccd977587fc66f8726ffd4f7
Aachintya <aachintya.20233001@mnnit.ac.in>
2024-11-01 05:49:34 +0530

    Removed Server folder

--- server/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
- import express from 'express';
-import { addTransaction, getTransactions } from '../controllers/transactionController.js';
-
-const router = express.Router();
-
-router.post('/add', addTransaction);  
-router.get('/:userId', getTransactions); 
-
-export default router;
